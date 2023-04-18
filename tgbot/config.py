18e95d50e64ced665ae28ebb504d2c05149280a0
Daniel Ángel <68783745+danielangelarro@users.noreply.github.com>
2023-04-17 20:35:33 -0400

    Create config.py

---
+++ tgbot/config.py
@@ -0,0 +1,17 @@
+# general configs
+
+TOKEN = ''
+DATABASE = 'systemd/sqlite.db'
+CHANNEL_PUBLIC_URL = ''
+CHANNEL_PRIVATE_URL = ''
+
+# games configs
+
+DICE_MULTIPLIER = 5
+TALL_MULTIPLIER = 2.05
+DBOMB_MULTIPLIER = 1.17
+
+# package configs
+
+__name__ = 'DiceFamilyBot'
+__version__ = '1.0'
